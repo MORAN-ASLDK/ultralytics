e10092ffa3396ee0ae96901b58f2d26bca5eb00a
MORAN-ASLDK <3088963166@qq.com>
2026-05-19 14:42:16 +0800

    Update train.py

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from ultralytics import YOLO
 
 
 if __name__ == '__main__':
-    model = YOLO('CSMA-YOLO.yaml')
+    model = YOLO('CSMS-YOLO.yaml')
     # model.load('yolo11n.pt') # loading pretrain weights
     model.train(data='/root/dataset/dataset_visdrone/data.yaml',
                 cache=False,
@@ -16,7 +16,7 @@ if __name__ == '__main__':
                 batch=32,
                 close_mosaic=0, # 最后多少个epoch关闭mosaic数据增强，设置0代表全程开启mosaic训练
                 workers=4, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
-                # device='0,1', 
+                device='0', 
                 optimizer='SGD', # using SGD
                 # patience=0, # set 0 to close earlystop.
                 # resume=True, # 断点续训,YOLO初始化时选择last.pt
